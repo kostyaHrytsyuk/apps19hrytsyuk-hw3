0ba8a278aaaf8595b843de02a76fb7ae3cbffd00
kostya.hrytsyuk <streetman889@gmail.com>
2019-11-16 17:11:46 +0200

    Code style improvements

--- src/main/java/ua/edu/ucu/smartarr/SmartArrayDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/SmartArrayDecorator.java
@@ -2,21 +2,19 @@ package ua.edu.ucu.smartarr;
 
 abstract class SmartArrayDecorator implements SmartArray {
 
+    private SmartArray smartArray;
+
+    public SmartArrayDecorator(SmartArray incomeSmartArray) {
+        this.smartArray = incomeSmartArray;
+    }
+
     public SmartArray getSmartArray() {
         return smartArray;
     }
 
-    public void setSmartArray(SmartArray smartArray) {
-        this.smartArray = smartArray;
+    public void setSmartArray(SmartArray incomeSmartArray) {
+        this.smartArray = incomeSmartArray;
     }
-
-    private SmartArray smartArray;
-
-    public SmartArrayDecorator(SmartArray smartArray) {
-        this.smartArray = smartArray;
-    }
-
-
 
     @Override
     public int size() {
